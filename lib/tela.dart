980d0f566623e658adc5f930496f43bd86d8583f
Jeffc-98 <jefferson_am1998@hotmail.com>
2026-04-30 19:58:06 -0400

    Melhorei a lógica da interface do app

--- lib/tela.dart
+++ lib/tela.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Tela extends StatefulWidget {
   const Tela({super.key});
 
@@ -8,74 +9,88 @@ class Tela extends StatefulWidget {
 }
 
 class _TelaState extends State<Tela> {
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina= TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-     appBar: AppBar(
-       backgroundColor: Colors.blue,
-       title: Text(
-           "Alcool ou Gasolina",
-           style: TextStyle(
-             fontSize: 25,
-             color: Colors.white,
-           ),
-       ),
-     ),
-
-      body: Column(
-        children: [
-          Column(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: Text("Alcool ou Gasolina",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white
+          ),
+        ),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch ,
             children: [
-              Padding(padding: EdgeInsets.all(25)),
-              Image.asset("imagens/logo.png")
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("imagens/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção de Combustível para seu carro",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço Alcool, ex: 1.59",
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool ,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço Gasolina, ex: 3.59",
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina ,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10) ,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(10),
+                      backgroundColor: Colors.cyan,
+                      foregroundColor: Colors.white
+                  ),
+                  onPressed: (){},
+                  child: Text("Calcular"),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top:20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
             ],
           ),
-          Padding(padding: EdgeInsets.all(32),
-           child: TextField(
-             keyboardType: TextInputType.number,
-             decoration: InputDecoration(
-               focusedBorder: UnderlineInputBorder(
-                 borderSide: BorderSide(color: Colors.black)
-               ),
-               enabledBorder: UnderlineInputBorder(
-                 borderSide: BorderSide(color: Colors.blue)
-               ),
-               labelText: "Preço do Alcool, ex: 1,59:",
-               labelStyle: TextStyle(
-                 color: Colors.black
-               )
-             ),
-           ),
-          ),
-          Padding(padding: EdgeInsets.all(32),
-            child: TextField(
-
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black)
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue)
-                  ),
-                  labelText: "Preço da Gasolina, ex: 3,15:",
-                  labelStyle: TextStyle(
-                      color: Colors.black
-                  )
-              ),
-            ),
-          ),
-          ElevatedButton(
-              onPressed: (){},
-              child: Text("Salvar"),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white
-            ),
-          ),
-        ],
-      ),
+        ) ,
+      ) ,
     );
   }
 }
